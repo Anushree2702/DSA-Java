8d4f1070def9030ae6ff4a071e049541bcf91756
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-08-02 22:44:02 +0530

    Add files via upload

---
+++ searching/Linear_search/first_last_index_of_element.java
@@ -0,0 +1,56 @@
+package searching.Linear_search;
+
+public class first_last_index_of_element {
+    static int[] searchRange(int[] nums, int target) {
+        
+        int i;
+        int j;
+        if(nums.length==0)
+        {
+            return new int[]{-1,-1};
+        }
+        if(search(nums,target))
+        {
+
+        for (i=0; i<nums.length ; i++ )
+        
+        {
+           
+            if(nums[i]==target)
+            {
+               
+               break;
+            }
+        }
+        for( j=nums.length-1; j>0 ; j--)
+        {
+            if(nums[j]==target)
+            {
+              
+                break;
+            }
+        }
+        return new int[]{i,j};
+        }
+        return new int[]{-1,-1} ;
+        
+    }
+     static boolean search(int[] nums, int target)
+    {
+       for(int a=0 ;a<nums.length ; a++ )
+       {
+        if (target==nums[a])
+        {
+            return true;
+        }
+       }
+       return false;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target=8;
+        System.out.println(searchRange(nums, target));
+    }
+    
+}
