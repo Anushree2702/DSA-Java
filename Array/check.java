9811bf3a7cc67ebd629cc32121cde1a141c9103c
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-08-03 13:49:17 +0530

    Add files via upload

---
+++ Array/check.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class check {
+    public static void main(String[] args) {
+        int j=0;
+        int c=100;
+        for(int i=0;i<2;i++)
+        {
+             System.out.println(j);
+             j++;
+        }
+        System.out.println(j);
+
+        {
+             c=10;
+        }
+        System.out.println(c);
+    }
+    
+}
