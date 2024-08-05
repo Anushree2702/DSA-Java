da06a540d73f14638b6035ee44f590f070487468
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-08-06 01:38:13 +0530

    Add files via upload

---
+++ searching/Binary_Search/optimized_firstandLast_Position_of_element.java
@@ -0,0 +1,56 @@
+package searching.Binary_Search;
+
+public class optimized_firstandLast_Position_of_element {
+    static int[] searchRange(int[] nums, int target) {
+
+        int[] ans={-1,-1};
+        ans[0]=search(nums,target,true);
+        if(ans[0]!=-1)
+        {
+        ans[1]=search(nums,target,false);
+        }
+        
+       
+        return ans;
+
+       
+        
+    }
+    static int search(int[] nums , int target , boolean findstartIndex)
+    {
+        int ans=-1;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(target<nums[mid])
+            {
+                end=mid-1;
+            }
+            else if(target>nums[mid])
+            {
+                start=mid+1; 
+            }
+            else
+            {
+               ans=mid;
+               if(findstartIndex)
+               {
+                end=mid-1;
+               }
+               else
+               {
+                start=mid+1;
+               }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums={5,7,7,8,8,10};
+        int target=8;
+        System.out.println(searchRange(nums, target));
+    }
+    
+}
