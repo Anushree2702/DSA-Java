a3186d978adebaaa8cc55f666d23e2c04e201e31
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-09-08 00:02:19 +0530

    Update bubble_sort.java

--- sorting/bubble_sort.java
+++ sorting/bubble_sort.java
@@ -1,13 +1,20 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class bubble_sort {
     public static void main(String[] args) {
+        int[] nums={5,4,3,2,1};
+        bubble(nums);
+        System.out.print(Arrays.toString(nums));
         
     }
     static void bubble(int[] nums)
+    
     {
         for(int i=0 ; i<nums.length ; i++)
         {
+            boolean swapped=false;
             for(int j=1 ; j<nums.length-i ; j++)
             {
                 if(nums[j]<nums[j-1])
@@ -15,6 +22,7 @@ public class bubble_sort {
                     int temp=nums[j];
                     nums[j]=nums[j-1];
                     nums[j-1]=temp;
+                    swapped=true;
                     
                     
                 }
