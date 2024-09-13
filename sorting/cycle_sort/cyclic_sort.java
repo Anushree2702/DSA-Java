331f61d2791c9ed2dbe025ea7f0ac14bf7ecbaca
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-09-13 22:34:20 +0530

    Add files via upload

---
+++ sorting/cycle_sort/cyclic_sort.java
@@ -0,0 +1,31 @@
+package sorting.cycle_sort;
+
+import java.util.Arrays;
+
+public class cyclic_sort {
+    
+    static void sort(int[] nums)
+    {
+        int i=0;
+        while(i<nums.length)
+        {
+            int correct=nums[i]-1;
+            if( nums[i]!=nums[correct])
+            {
+                int temp=nums[i];
+                nums[i]=nums[correct];
+                nums[correct]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums={3,0,5,2,1,4};
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    
+}
