208c872018e5a0389c952afd091dbfeeb4047a01
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-09-16 23:08:51 +0530

    Add files via upload

---
+++ Strings/New_keyword.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class New_keyword {
+    public static void main(String[] args) {
+        String a="hello";
+        String b="hello";
+        System.out.println(a==b);
+
+        String c=new String("welcome");
+        String d=new String("welcome");
+        System.out.println(c==d);
+    }
+    
+}
