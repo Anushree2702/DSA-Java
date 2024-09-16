b848fc4df6572ba9913192a02aecbe009454e215
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-09-16 22:49:17 +0530

    Add files via upload

---
+++ Strings/Comparison.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a="hello";
+        String b="hello";
+        System.out.println(a==b);
+        
+    }
+   
+    
+    
+}
