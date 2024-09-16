30e3585baa068bd72bc979feea842dcf82f52ed0
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-09-16 23:28:24 +0530

    Add files via upload

---
+++ Strings/Methods.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class Methods {
+    public static void main(String[] args) {
+
+        //Equals:
+        //str1.equals(str2) - Checks if two strings are equal.
+
+        String c=new String("welcome");
+        String d=new String("welcome");
+        System.out.println(c.equals(d));
+
+
+
+        ////CharAt: 
+        //str.charAt(index) - Returns the character at the specified index.
+
+        System.out.println(c.charAt(1));
+
+        System.out.println(56);
+    }
+    
+}
